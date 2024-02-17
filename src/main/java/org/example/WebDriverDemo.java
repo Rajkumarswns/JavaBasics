05e5f655cbb7872693434904a4a237e5c680fbe1
Rajkumar Sankaranarayanan <rajkumars1971@hotmail.com>
2024-02-17 23:03:48 +0530

    DemoformCreated

--- src/main/java/org/example/WebDriverDemo.java
+++ src/main/java/org/example/WebDriverDemo.java
@@ -1,44 +1,161 @@
 package org.example;
 
+import java.io.Console;
 import java.time.Duration;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class WebDriverDemo {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
 
+        try
+        {
         // create webdriver object to use chrome browser
         WebDriver driver = new ChromeDriver();
 
         // configure implicit wait
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.get("https://www.google.com");
-        driver.findElement(By.name("q")).sendKeys("Selenium");
-        //hit enter key
-        driver.findElement(By.name("q")).submit();
+        driver.get("http://127.0.0.1:3000/src/main/java/org/example/demo.html");
+        Thread.sleep(5000);
 
-        // add explicit wait
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }   
-        //verify the title of the browser
         System.out.println(driver.getTitle());
 
         // assert the title "Selenium - Google Search"
-        if(driver.getTitle().equals("Selenium - Google Search")){
+        if(driver.getTitle().equals("Selenium Locator Strategy Demo Site")){
             System.out.println("Test Passed");
             // use assert to fail the test
             assert true;
         }
 
+
+        // Find web element by id
+        WebElement EmployeeID = driver.findElement(By.id("employeeId"));
+        if(EmployeeID.isDisplayed()){
+            // write message to console
+            System.out.println("Employee ID is displayed");
+             EmployeeID.clear();
+            EmployeeID.sendKeys("12345");  
+            assert true; 
+        }
+
+        // Find Web element by name
+        WebElement EmployeeName = driver.findElement(By.name("employeeName"));
+        if(EmployeeName.isDisplayed()){
+            // write message to console
+            System.out.println("Employee Name is displayed");
+            EmployeeName.clear();
+            EmployeeName.sendKeys("John Doe"); 
+            assert true;
+        }
+
+        // Get the dob element whose type is input
+        WebElement dob = driver.findElement(By.xpath("//input[@type='date']"));
+        if(dob.isDisplayed()){
+
+              // write message to console
+            System.out.println("Date of Birth is displayed");
+            dob.clear();
+            dob.sendKeys("02/02/2017");
+             assert true;
+        }
+        
+        // get the Male from the Gender Dropdown
+        WebElement Gender = driver.findElement(By.id("GenderInput"));
+
+        if (Gender.isDisplayed()) {
+            // write message to console
+            System.out.println("Gender drop down is displayed");
+            // Create Select object
+            Select genderSelect = new Select(Gender);
+            // Select the option with Male
+            genderSelect.selectByVisibleText("Male");
+        }
+
+        WebElement langRadioButton   = driver.findElement(By.id("html"));
+        if(langRadioButton.isDisplayed()){
+            // write message to console
+            System.out.println("Language radio button is displayed");
+            langRadioButton.click();
+            assert true;
+        }
+
+        // locate the url with the partial text oo and click
+        WebElement url = driver.findElement(By.linkText("Yahoo"));
+        System.out.println(url.getAttribute("href"));
+
+        if(url.isDisplayed()){
+            // write message to console
+            System.out.println("URL is displayed");
+            var url1 = url.getAttribute("href");
+
+            // open the url in a seperate tab
+            JavascriptExecutor js = (JavascriptExecutor) driver;
+            
+            // open the url1 in the new tab
+            js.executeScript("window.open('"+url1+"')");
+            
+            // click on the first tab
+            driver.switchTo().window(driver.getWindowHandle());
+
+            assert true;
+        }
+
+        // get the address1 element from iframe with the id addressiframe
+        driver.switchTo().frame("addressiframe");
+        WebElement address1 = driver.findElement(By.id("address1"));
+        if(address1.isDisplayed()){
+            // write message to console
+            System.out.println("Address1 is displayed");
+            address1.clear();
+            address1.sendKeys("1234 Main Street");
+            assert true;
+        }   
+
+        // switch back to the main window
+        //driver.switchTo().defaultContent();
+        driver.switchTo().window(driver.getWindowHandle());
+
+        
+        // click on submit button
+        WebElement submitButton = driver.findElement(By.xpath("//button[@type='submit']"));
+        if(submitButton.isDisplayed()){
+            // write message to console
+            System.out.println("Submit button is displayed");
+            submitButton.click();
+            assert true;
+
+            // click on the alert window
+             
+            System.out.println("Alert message is displayed");
+
+            driver.switchTo().alert().accept();
+            assert true;
+
+
+        }
           
+
+
+ 
+        Thread.sleep(3000);
+
+        // add explicit wait
+    
         driver.quit();
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }   
+        //verify the title of the browser
+       
+          
         
         
     }
--- src/main/java/org/example/WebDriverDemo1.java
+++ src/main/java/org/example/WebDriverDemo1.java
@@ -1,9 +1,13 @@
 package org.example;
 
+import java.net.URI;
+import java.net.URL;
+
 import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class WebDriverDemo1 {
@@ -13,54 +17,77 @@ public class WebDriverDemo1 {
     public static void main(String[] args)
     {
         // Create ChromeWebDriver
-        WebDriver driver = new ChromeDriver();
 
-        // what is browser capabilities
-        // 1. it helps to set the properties of browser
-        // 2. it helps to get the properties of browser
-        // 3. below are few properties that can be set or get
-        // 4. browser name, browser version, platform, platform name, platform version, acceptInsecureCerts, pageLoadStrategy, proxy, setWindowRect, timeouts, unhandledPromptBehavior
-        // set browser name
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setBrowserName("chrome");
+        capabilities.setVersion("latest");
+       // capabilities.setPlatform(org.openqa.selenium.Platform.WINDOWS);
+
+        var url = "http://localhost:4444/";
+        // convert url to URI
+        URI uri = URI.create(url);
+ 
+        try{
+            // create RemoteWebDriver with capabilities
+            RemoteWebDriver rw  = new RemoteWebDriver (new URL(url), capabilities);
+            rw.get("https://www.google.com");
+            System.out.println(rw.getTitle());
+            rw.quit();
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+     
+      
+        // WebDriver driver = new ChromeDriver();
+
+
+        
+
+        // // what is browser capabilities
+        // // 1. it helps to set the properties of browser
+        // // 2. it helps to get the properties of browser
+        // // 3. below are few properties that can be set or get
+        // // 4. browser name, browser version, platform, platform name, platform version, acceptInsecureCerts, pageLoadStrategy, proxy, setWindowRect, timeouts, unhandledPromptBehavior
+        // // set browser name
      
     
 
 
-        // List all default capabilities of driver
+  
 
-        Capabilities    capabilities = ((RemoteWebDriver) driver).getCapabilities();
-        System.out.println(capabilities);
-
-        // from capabilities parse it as json and print key and value
+        // // from capabilities parse it as json and print key and value
  
-        var res = capabilities.asMap();
-        for (var entry : res.entrySet()) {
-            System.out.println(entry.getKey() + " : " + entry.getValue());
-        }
+        // var res = capabilities.asMap();
+        // for (var entry : res.entrySet()) {
+        //     System.out.println(entry.getKey() + " : " + entry.getValue());
+        // }
 
-        // maximize the browser window
+        // // maximize the browser window
  
-        // create ChromeOptions and set the details
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-extensions");
-        options.addArguments("--disable-popup-blocking");
-        options.addArguments("--start-maximized");
+        // // create ChromeOptions and set the details
+        // ChromeOptions options3 = new ChromeOptions();
+        // options3.addArguments("--disable-extensions");
+        // options3.addArguments("--disable-popup-blocking");
+        // options3.addArguments("--start-maximized");
 
-        // create ChromeWebDriver with options
-        WebDriver driver1 = new ChromeDriver(options);
+        // // create ChromeWebDriver with options
+        // WebDriver driver1 = new ChromeDriver(options3);
 
-        driver1.get("https://www.google.com");
-        driver1.manage().window().maximize();
+        // driver1.get("https://www.google.com");
+        // driver1.manage().window().maximize();
 
-        System.out.println(driver1.getTitle());
-        System.out.println(driver1.getCurrentUrl() ); 
-        driver1.navigate().to("https://www.facebook.com");
-        driver1.navigate().back();
-        driver1.navigate().forward();
-        driver1.navigate().refresh();
+        // System.out.println(driver1.getTitle());
+        // System.out.println(driver1.getCurrentUrl() ); 
+        // driver1.navigate().to("https://www.facebook.com");
+        // driver1.navigate().back();
+        // driver1.navigate().forward();
+        // driver1.navigate().refresh();
 
 
-        driver.quit();
-        driver1.quit();
+        // driver.quit();
+        // driver1.quit();
 
         // WebDriver Methods 
         // get() : Load a new web page in the current browser window. This is done using an HTTP GET operation, and the method will block until the load is complete.   
