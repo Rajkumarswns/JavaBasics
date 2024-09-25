871990d23b49692a9abe4d3ebe239d848e6b6851
Rajkumar Sankaranarayanan <rajkumars1971@hotmail.com>
2024-09-25 21:48:34 +0530

    Relative locator update

--- src/main/java/org/example/Internal/LocatorStrategyDemo.java
+++ src/main/java/org/example/Internal/LocatorStrategyDemo.java
@@ -15,86 +15,74 @@ import org.openqa.selenium.support.pagefactory.ByAll;
 import org.openqa.selenium.support.pagefactory.ByChained;
 
 public class LocatorStrategyDemo {
-    
-    public static void main(String argc[])
-    {
 
- // create chrome options  https://peter.sh/experiments/chromium-command-line-switches/
+   public static void main(String argc[]) {
 
- try{
-            ChromeOptions options = new ChromeOptions();
-            options.addArguments("--disable-notifications");
-            // create webdriver object to use chrome browser
-            WebDriver driver = new ChromeDriver();
+      // create chrome options
+      // https://peter.sh/experiments/chromium-command-line-switches/
 
-            // maximize the browser window
-            driver.manage().window().maximize();
+      try {
+         ChromeOptions options = new ChromeOptions();
+         options.addArguments("--disable-notifications");
+         // create webdriver object to use chrome browser
+         WebDriver driver = new ChromeDriver();
 
-            // configure implicit wait
-            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-            driver.get("http://127.0.0.1:3000/src/main/java/org/example/demo.html");
-            Thread.sleep(1000);
+         // maximize the browser window
+         driver.manage().window().maximize();
 
+         // configure implicit wait
+         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+         driver.get("http://127.0.0.1:3000/JavaBasics/src/main/java/org/example/demo.html");
+         Thread.sleep(1000);
 
-// compound locators
+         // compound locators
 
-        // By Id or Name
-           WebElement empName = driver.findElement(new ByIdOrName("employeeName"));
-           empName.sendKeys("Rajkumar");
+         // By Id or Name
+         WebElement empName = driver.findElement(new ByIdOrName("employeeName"));
+         empName.sendKeys("Rajkumar");
 
-           // By Chained
-           List<WebElement> tableRows = driver.findElements(new ByChained(By.tagName("table"),
-           By.tagName("tbody"), By.tagName("tr")));
+         // By Chained
+         List<WebElement> tableRows = driver.findElements(new ByChained(By.tagName("table"),
+               By.tagName("tbody"), By.tagName("tr")));
 
-           System.out.println(tableRows.size());
-           for(WebElement row : tableRows)
-           {
-                    System.out.println(row.getText());
-           }
+         System.out.println(tableRows.size());
+         for (WebElement row : tableRows) {
+            System.out.println(row.getText());
+         }
 
-           // By All
-           List<WebElement> radiobuttonElements = driver.findElements(new ByAll(By.tagName("input"),
-           By.cssSelector("[type='color']"),By.name("my-colors")));
+         // By All
+         List<WebElement> radiobuttonElements = driver.findElements(new ByAll(By.tagName("input"),
+               By.cssSelector("[type='color']"), By.name("my-colors")));
 
-           System.out.println(radiobuttonElements.size());
-           for(WebElement element : radiobuttonElements)
-           {
-                    System.out.println(element.isDisplayed());
-           }
+         System.out.println(radiobuttonElements.size());
+         for (WebElement element : radiobuttonElements) {
+            System.out.println(element.isDisplayed());
+         }
 
-           // relative
+         // relative
 
-           // Locate the Date of Birth input field
-           WebElement dobInput = driver.findElement(By.xpath("//input[@type='date' and @class='form-control']"));
+         // Locate the Date of Birth input field
+         WebElement dobInput = driver.findElement(By.xpath("//input[@type='date' and @class='form-control']"));
 
-           // Locate Employee Name input field using Relative Locator
-           WebElement employeeNameInput = driver.findElement(
-                   RelativeLocator.with(By.tagName("input")).above(dobInput));
+         // Locate Employee Name input field using Relative Locator
+         WebElement employeeNameInput = driver.findElement(
+               RelativeLocator.with(By.tagName("input")).above(dobInput));
 
-           System.out.println(employeeNameInput.getAttribute("placeholder"));
+         System.out.println(employeeNameInput.getAttribute("placeholder"));
 
-           // wait for 10 seconds
-            dobInput = driver.findElement(By.xpath("//input[@type='date' and @class='form-control']"));
+         // Locate Select Gender dropdown using Relative Locator
+         WebElement selectGenderDropdown = driver.findElement(
+               RelativeLocator.with(By.tagName("select")).near(dobInput));
 
-   // Locate Select Gender dropdown using Relative Locator
-   WebElement selectGenderDropdown = driver.findElement(
-      RelativeLocator.with(By.tagName("select")).below(dobInput));
+         // Print the ID attribute value to ensure correct element is located
+         System.out.println(selectGenderDropdown.getAttribute("id"));
 
-// Print the ID attribute value to ensure correct element is located
-System.out.println(selectGenderDropdown.getAttribute("id"));
-            
+         
+         driver.quit();
 
-           // Print the placeholder attribute value to ensure correct element is located
-          // 
-           
+      } catch (Exception ex) {
+         System.out.println(ex.getMessage());
+      }
 
-            driver.quit();
-
-
- }catch(Exception ex)
- {
-    System.out.println(ex.getMessage());
- }
-
-    }
+   }
 }
